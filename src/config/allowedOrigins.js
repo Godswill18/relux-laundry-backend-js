d57494c72b63b22a352c0b6b6bd868bd5db42d21
Relux Dev <dev@reluxlaundry.com>
2026-03-27 21:12:58 +0100

    cors added

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     // 'http://192.168.0.7:8080',
     'http://localhost:8080',
-    'http://localhost:8081'
+    'http://localhost:8081',
+    'https://relux.ng',
+    'https://staff.relux.ng'
     // 'http://localhost:5173',
     // 'http://localhost:5174',
     // 'http://localhost:4173'
