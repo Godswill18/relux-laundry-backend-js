5f00923bef3e473dbe418bb1ff75eba7f87b54a6
Relux Dev <dev@reluxlaundry.com>
2026-04-20 16:24:44 +0100

    updated

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -10,6 +10,7 @@ const allowedOrigins = [
     'https://relux.ng',
     'https://staff.relux.ng',
     'https://api.authentication.relux.relux.ng',
+    'https://api.authentication.relux.relux.ng/uploads/announcements',
     'https://admin.relux.ng',
 ];
 
